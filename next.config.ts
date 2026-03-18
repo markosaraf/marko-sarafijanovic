e6c26ae2d07b591d0895ac41121ac423f4428346
markosaraf <118335343+markosaraf@users.noreply.github.com>
2026-03-18 14:39:37 +0100

    Add redirect from www to non-www in config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,22 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  async redirects() {
+    return [
+      // Redirect www to non-www (http)
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.marko-sarafijanovic.com',
+          },
+        ],
+        destination: 'https://marko-sarafijanovic.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
